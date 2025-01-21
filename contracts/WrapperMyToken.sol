0749103c0a32d0d9ffff358e06eefaf1ff681ab7
wangxinyu <alexdevw@gmail.com>
2025-01-21 20:10:21 +0800

    feat: init

---
+++ contracts/WrapperMyToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {MyToken} from "./MyToken.sol";
+
+contract WrapperMyToken is MyToken {
+    constructor(
+        string memory TokenName,
+        string memory TokenSymbol
+    ) MyToken(TokenName, TokenName) {}
+
+    function mintTokenWithSpecificTokenId(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
+}
